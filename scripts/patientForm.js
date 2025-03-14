3e4c356cb05bec95a186276884f93dd316fe7072
SebastianMuzalewski <sebmuzalew@gmail.com>
2025-03-14 14:22:24 -0400

    Finalized Alerts for Patient Form & Patient Search/Modify

--- scripts/patientForm.js
+++ scripts/patientForm.js
@@ -12,7 +12,11 @@ function subClick(){
     
     document.getElementById('alertMsg').style.display = 'block';
 
-    document.getElementById('closebtn').onclick = function() {
+    document.getElementById('btnClose').onclick = function() {
+        document.getElementById('alertMsg').style.display='none';
+    };
+
+    document.getElementById('cornerClose').onclick = function() {
         document.getElementById('alertMsg').style.display='none';
     };
     
--- scripts/patientSearchModify.js
+++ scripts/patientSearchModify.js
@@ -7,3 +7,28 @@ function toggleNavbar() {
     navbar.classList.toggle('active'); 
     body.classList.toggle('navbar-closed');
 }
+
+function subDeleteClick(){
+    
+    document.getElementById('alertMsgWarn').style.display = 'block';
+
+    document.getElementById('btnCloseNo').onclick = function() {
+        document.getElementById('alertMsgWarn').style.display='none';
+    };
+
+    document.getElementById('btnCloseYes').onclick = function() {
+        document.getElementById('alertMsgWarn').style.display='none';
+        document.getElementById('alertMsgDelete').style.display='block';
+    };
+
+    document.getElementById('btnClose').onclick = function() {
+        document.getElementById('alertMsgDelete').style.display='none';
+    };
+
+    document.getElementById('cornerCloseWarn').onclick = function() {
+        document.getElementById('alertMsgWarn').style.display='none'; 
+    };
+    document.getElementById('cornerCloseDelete').onclick = function() {
+        document.getElementById('alertMsgDelete').style.display='none'; 
+    };
+}
