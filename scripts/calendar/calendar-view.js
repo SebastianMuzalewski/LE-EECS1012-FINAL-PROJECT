798d27d96d2b83c8542c0dcb0acd55eda7bd83f4
DAVID CAO <caothecongdanh@gmail.com>
2025-03-22 18:03:01 -0400

    month and week event calendar including selector function next update is display event and fix the layout

---
+++ scripts/calendar/calendar-view.js
@@ -0,0 +1,19 @@
+export function initCalendar(){
+    const monthCalendarElement= document.querySelector("[data-month-calendar]")
+    const weekCalendarElement= document.querySelector("[data-week-calendar]")
+    document.addEventListener("view-change",
+        (event)=>{
+            const selectedView=event.detail.view;
+            if(selectedView==="month-selector"){
+                monthCalendarElement.style.display="flex";
+                weekCalendarElement.style.display="none";
+            }else if(selectedView=="week-selector"){
+                weekCalendarElement.style.display="flex";
+                monthCalendarElement.style.display="none";
+            }else{
+                weekCalendarElement.style.display="none";
+                monthCalendarElement.style.display="none";
+            }
+        }
+    );
+}
--- scripts/calendar/calendar.js
+++ scripts/calendar/calendar.js
@@ -1,5 +1,10 @@
 // Toggles Navbar if icon is clicked
 // Shifts the header if the navbar is open
+import {initViewSelect} from "./viewselect.js";
+import {initCalendar} from  "./calendar-view.js";
+
+initCalendar();
+initViewSelect();
 function toggleNavbar() {
     let navbar = document.getElementById('navbarToggle');
     let body = document.body;
